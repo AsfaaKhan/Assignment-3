10b4a7852deb13408785bc245f6a8b4aba3148e7
AsfaaKhan <khanzadiasfi0314@gmail.com>
2024-10-22 20:00:08 +0500

    Initial Commit

--- app/Components/VisitSection/places.tsx
+++ app/Components/VisitSection/places.tsx
@@ -6,17 +6,27 @@ const Places =() =>{
         <div id="places">
             <hr />
             <div className="mt-20 items-center">
-                <h1 className="text-center text-6xl font-serif">Visiting Places</h1>
-                <h1 className="text-center pt-10 text-2xl underline ">
-                    <a href="/VisitingPlaces" target="_blank" className="hover:text-purple-600" >
-                        Winter Holidays You are Dream About
-                    </a>
-                </h1>
-                
-                <div className="pt-16 place-items-center ">
-                <Image src={visitPlaces} width={600}  height={500} alt="visitImage" className="content-center"/>
-                </div>
-            </div>
+  <h1 className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-serif">
+    Visiting Places
+  </h1>
+  <h1 className="text-center pt-8 sm:pt-10 text-xl sm:text-2xl underline">
+    <a
+      href="/VisitingPlaces"
+      target="_blank"
+      className="hover:text-purple-600"
+    >
+      Winter Holidays You are Dreaming About
+    </a>
+  </h1>
+
+  <div className="pt-12 sm:pt-16 flex justify-center">
+    <Image src={visitPlaces} width={400} height={300}
+      className=" sm:w-96 md:w-[500px] lg:w-[600px]"
+      alt="visitImage"
+    />
+  </div>
+</div>
+
 
 
 
