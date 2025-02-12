fccbad10200c867bfbd61d5bd7433fe5dda65e92
Asfaa Khan <159608757+AsfaaKhan@users.noreply.github.com>
2025-02-12 21:43:01 +0500

    Update SubAbout.tsx

--- components/SubAbout.tsx
+++ components/SubAbout.tsx
@@ -12,19 +12,19 @@ export default function SubAbout() {
 
 
                 {/* Section 1 */}
-                <div className=" container flex flex-col justify-center items-center  gap-8 md:flex-row lg:flex-row md:justify-between lg:justify-between xl:justify-between md:items-center  pt-5 lg:pt-6 pb-16  ">
+                <div className=" container flex flex-col justify-center items-center  gap-8 md:flex-row  md:justify-between  md:items-center  pt-5 lg:pt-6 pb-16  ">
                     <div className=" flex flex-col justify-center   gap-6">
-                        <h1 className="text-xl sm:text-2xl underline pb-4 pt-8 lg:pt-12">
+                        <h1 className="text-lg sm:text-xl underline pb-4 pt-8 lg:pt-12">
                             What Do you know about our website?
                         </h1>
-                        <p className="w-[350px] lg:w-[450px] ">
+                        <p className=" ">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis commodi illo quidem corporis mollitia? Ab eum tempora voluptatum quae aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur architecto fugiat rem earum fuga similique. Quaerat laboriosam maxime aperiam nisi?
                         </p>
 
                         <h1 className="text-xl sm:text-2xl underline pb-4 pt-8 lg:pt-12">
                             What Do you know about our website?
                         </h1>
-                        <p className="w-[350px] lg:w-[450px] ">
+                        <p className=" ">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis commodi illo quidem corporis mollitia? Ab eum tempora voluptatum quae aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur architecto fugiat rem earum fuga similique. Quaerat laboriosam maxime aperiam nisi?
                         </p>
                     </div>
